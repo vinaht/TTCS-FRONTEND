2d1dfe26a2f2906660362db90b40b0049e252097
vinaht <lzvanh7@gmail.com>
2026-05-17 00:12:26 +0700

    update

--- js/shared.js
+++ js/shared.js
@@ -1,5 +1,5 @@
 (function () {
-    const API_BASE_URL = window.CUBEAL_API_BASE_URL || "http://localhost:5000/api";
+    const API_BASE_URL = window.CUBEAL_API_BASE_URL || "https://cubeal.up.railway.app/api";
     const API_ORIGIN = new URL(API_BASE_URL, window.location.href).origin;
     const sidebarItems = {
         home: {
